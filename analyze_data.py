9b29703e7d617b8aa9ac10346ec0b8419472a634
autommen <automme.nico@gmail.com>
2023-11-15 12:14:34 +0100

    Refactor analyze data

--- analyze_data.py
+++ analyze_data.py
@@ -1,10 +1,36 @@
-import pandas
+import pandas as pd
+import os
+
 
 if __name__ == '__main__':
-    #dev = pandas.read_xml('data/dev.xml')
-    #dev.to_csv('data/dev.csv', index=False)
 
-    """train = pandas.read_xml('data/train.xml')
-    train = train.head(100400)
-    train.to_csv('data/train_tronc.csv', index=False)"""
+    if not os.path.exists('data/dev.csv'):
+        document = pd.read_xml('data/dev.xml')
+
+        dictUser = {}
+        count = 0
+
+        # Parcourir les lignes du DataFrame
+        for index, row in document.iterrows():
+            # Lire la valeur actuelle dans la colonne spécifiée
+            user_id = row["user_id"]
+            note = row["note"]
+
+            if user_id not in dictUser:
+                dictUser[user_id] = count
+                count = count + 1
+
+            document.at[index, "user_id"] = dictUser[user_id]
+            document.at[index, "note"] = float(note.replace(',', '.'))
+            document.at[index, "len_comment"] = len(str(row['commentaire']))
+
+        # Sauvegarder le DataFrame modifié dans un nouveau fichier CSV
+        document.to_csv('data/dev.csv', index=False)
+
+    if not os.path.exists('data/train.csv'):
+        document = pd.read_xml('data/train.xml')
+        document = document.head(100400)
+        document.to_csv('data/train.csv', index=False)
+
+
 
