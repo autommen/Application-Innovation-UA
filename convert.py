63277be766239ddd940092d18186de8d7c72dfe8
PikaManon <manon.samson@univ-avignon.fr>
2023-11-15 12:07:48 +0100

    change convert

--- convert.py
+++ convert.py
@@ -3,20 +3,15 @@ import pandas as pd
 import csv
 
 if __name__ == '__main__':
-    #document = pandas.read_xml('data/dev.xml')
-    #document.to_csv('data/dev.csv', index=False)
+    """document = pd.read_xml('data/dev.xml')
+    document.to_csv('data/dev.csv', index=False)
     
-    """train = pandas.read_xml('data/train.xml')
+    train = pd.read_xml('data/train.xml')
     train = train.head(100400)
     train.to_csv('data/train_tronc.csv', index=False)"""
 
     dictUser = {}
     count = 0
-
-    with open('data/dev.csv', 'r', encoding="utf-8") as file:
-        reader = csv.reader(file, delimiter=',')
-        for row in reader:
-            print(row)
 
 
      # Charger le fichier CSV dans un DataFrame
@@ -27,16 +22,17 @@ if __name__ == '__main__':
     for index, ligne in df.iterrows():
         # Lire la valeur actuelle dans la colonne spécifiée
         valeur_actuelle = ligne["user_id"]
+        valeur_actuelle_note = ligne["note"]
         
         if valeur_actuelle not in dictUser :
             dictUser[valeur_actuelle] = count
             count = count + 1
         
         df.at[index, "user_id"] = dictUser[valeur_actuelle]
-        #print(ligne['commentaire'])
-        #df.at[index, "len_comment"] = len(ligne['commentaire'])
+        df.at[index, "note"] = float(valeur_actuelle_note.replace(',', '.'))
+        df.at[index, "len_comment"] = len(str(ligne['commentaire']))
 
     
     # Sauvegarder le DataFrame modifié dans un nouveau fichier CSV
-    df.to_csv('data/dev_changeUser_rajoutLen.csv', index=False)
+    df.to_csv('data/dev_changeUser_changeNote.csv', index=False)
     
