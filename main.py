758a4dc2c73f0ec75bcb98831119942402542f1f
autommen <automme.nico@gmail.com>
2023-12-11 16:53:44 +0100

    Correction de l'explosion d'utilisation de la mémoire vive

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import os
 
 import sklearn
 from sklearn import svm
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nltk.corpus import stopwords
 from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
 
 
 def get_dataframe(filepath: str) -> DataFrame:
     if not os.path.exists(filepath + '.csv'):
         dataframe = pandas.read_xml(filepath + '.xml')
 
-        # Parcourir les lignes du DataFrame
-        for index, row in dataframe.iterrows():
-            dataframe.at[index, "note"] = float(row["note"].replace(',', '.'))
+        dataframe['note'].apply(lambda note: float(note.replace(',', '.')))
 
         # Sauvegarder le DataFrame modifié dans un nouveau fichier CSV
         dataframe.to_csv(filepath + '.csv', index=False)
@@ -23,29 +25,32 @@ def get_dataframe(filepath: str) -> DataFrame:
 
 
 if __name__ == '__main__':
-    path = 'data/dev'
-    document = get_dataframe(path)
+    path = 'data/train'
+    document = get_dataframe(path).dropna()
 
     text_data = document['commentaire']
-    text_data = text_data.fillna('')
 
-    tfidf_vectorizer = TfidfVectorizer()
-    x_tfidf = tfidf_vectorizer.fit_transform(text_data)
+    tfidf_vectorizer = TfidfVectorizer(stop_words=stopwords.words('french'), max_features=4000)
+    x_tfidf = tfidf_vectorizer.fit_transform(text_data.values)
 
-    other_features = document.drop(['note', 'commentaire'], axis=1)
-    x_combined = pandas.concat([other_features, pandas.DataFrame(x_tfidf.toarray())], axis=1)
+    #other_features = document.drop(['note', 'commentaire'], axis=1)
+    #x_combined = pandas.concat([other_features, pandas.DataFrame(x_tfidf.toarray())], axis=1)
+    #x = x_combined.columns.astype(str)
 
-    x = x_combined.columns.astype(str)
+    x = pandas.DataFrame(x_tfidf.toarray())
     y = document['note']
+
+    lab = LabelEncoder()
+    y = lab.fit_transform(y)
+
+    print(x,y)
 
     if len(x) == len(y):
         x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.25, random_state=42)
-
-        svc = svm.SVC(verbose=True)
+        svc = svm.LinearSVC(verbose=True)
         svc.fit(x_train, y_train)
         y_pred = svc.predict(x_test)
-        accuracy = sklearn.metrics.accuracy_score(y_test, y_pred) * 100
-        print("Accuracy: {:.2f}%".format(accuracy))
+        print(classification_report(y_test, y_pred))
     else:
         print("Error: Inconsistent number of samples between x ("+str(len(x))+") and y ("+str(len(y))+").")
 
